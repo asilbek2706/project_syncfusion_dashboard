a5e175cbc7a7b0fe7c75ebebc2ecc7216dce4c79
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-15 20:41:04 +0500

    Color Picker component implemented and four App components done

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+import { Header } from '../components';
+
+const change = (args) => {
+    document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+};
 
 const ColorPicker = () => {
-    return <div>ColorPicker</div>;
+    return (
+        <div className={'m-2 mt-24 rounded-3xl bg-white p-2 md:m-10 md:p-10'}>
+            <Header category={'App'} title={'Color Picker'} />
+            <div className='text-center'>
+                <div id='preview' />
+                <div className='flex flex-wrap items-start justify-center gap-20'>
+                    <div>
+                        <p className='mb-4 mt-2 text-2xl font-semibold'>Inline Palette</p>
+                        <ColorPickerComponent
+                            id='inline-palette'
+                            mode='Palette'
+                            modeSwitcher={false}
+                            inline
+                            showButtons={false}
+                            change={change}
+                        />
+                    </div>
+                    <div>
+                        <p className='mb-4 mt-2 text-2xl font-semibold'>Inline Picker</p>
+                        <ColorPickerComponent
+                            id='inline-picker'
+                            mode='Picker'
+                            modeSwitcher={false}
+                            inline
+                            showButtons={false}
+                            change={change}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ColorPicker;
