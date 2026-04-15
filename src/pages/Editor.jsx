a3602c3dc2ef735214f73c10fc7a3abf28b611fd
Asilbek2706 <249135167+Asilbek2706@users.noreply.github.com>
2026-04-15 20:28:08 +0500

    Editor component implemented

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import {
+    HtmlEditor,
+    Image,
+    Inject,
+    Link,
+    RichTextEditorComponent,
+    QuickToolbar,
+    Toolbar,
+} from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/dummy';
+
+import { Header } from '../components';
 
 const Editor = () => {
-    return <div>Editor</div>;
+    return (
+        <div className={'m-2 mt-24 rounded-3xl bg-white p-2 md:m-10 md:p-10'}>
+            <Header category={'App'} title={'Editor'} />
+            <RichTextEditorComponent>
+                <EditorData />
+                <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+            </RichTextEditorComponent>
+        </div>
+    );
 };
 
 export default Editor;
